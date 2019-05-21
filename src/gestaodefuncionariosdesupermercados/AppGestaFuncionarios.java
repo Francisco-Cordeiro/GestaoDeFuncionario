9bf1b5235f63567963849cce20436f9fc4f9abe2
Francisco Cordeiro <ak47ipg@gmail.com>
2019-05-21 15:35:48 +0100

    comite de AppGestaoDeFuncionariosDeSupermercados

--- src/gestaodefuncionariosdesupermercados/AppGestaFuncionarios.java
+++ src/gestaodefuncionariosdesupermercados/AppGestaFuncionarios.java
@@ -332,7 +332,7 @@ public class AppGestaFuncionarios extends javax.swing.JFrame {
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addContainerGap(367, Short.MAX_VALUE)
+                .addContainerGap(405, Short.MAX_VALUE)
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 263, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -389,11 +389,11 @@ public class AppGestaFuncionarios extends javax.swing.JFrame {
         jDialogPesquisaNome.getContentPane().setLayout(jDialogPesquisaNomeLayout);
         jDialogPesquisaNomeLayout.setHorizontalGroup(
             jDialogPesquisaNomeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jDialogPesquisaNomeLayout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jDialogPesquisaNomeLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jDialogPesquisaNomeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(jDialogPesquisaNomeLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
         jDialogPesquisaNomeLayout.setVerticalGroup(
